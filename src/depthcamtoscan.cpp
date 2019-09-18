618b6cbf7324a03bcf6f6e1d29d6a7fb1001e278
xaxxon <colin@xaxxon.com>
2019-09-17 20:55:48 -0700

    custom depthcam to scan with floor plane obstacles and auto horiz calibration

--- src/depthcamtoscan.cpp
+++ src/depthcamtoscan.cpp
@@ -10,12 +10,23 @@
 
 ros::Publisher pub;
 
-const float image_ignore_ratio_ = 0.675;
+double camFOVy;
+const float image_ignore_ratio_ = 0.4; // 0.675; // 0.675; // top of image ignore this ratio
 const float frame_z_ = 0.163;
-const double floorplane_obstacle_height_ = 0.075;
-const double floorplane_cliff_depth_ = 0.075;
-float horiz_angle_offset_ = 0;
-const int scan_height = 5;
+const double floorplane_obstacle_height_ = 0.04; // 0.075
+const double floorplane_cliff_depth_ = 0.5; // 0.075
+
+bool calibrationcomplete = false;
+int calibrationscancount = 0;
+float avghrzangleoffst = 0.0;
+float horiz_angle_offset_ = 0.0;
+bool framecalibrated;
+const float HRZCALRANGE = 0.12217; // +/- vert radians (7 deg)
+const float HRZCALINC = 0.00576; // 0.33 deg
+const int CALIBFRAMESMAX = 5;
+int framecount = 0;
+const int DROPINITFRAMES = 30;
+const float CALIBCONST = -0.03; // horiz_angle_offset_ distortion comp?
 
 
 bool use_point(const float new_value, const float old_value, 
@@ -62,14 +73,10 @@ double angle_between_rays(const cv::Point3d& ray1, const cv::Point3d& ray2) {
 }
 
 
-void imageCb(const sensor_msgs::ImageConstPtr& depth_msg, 
-   const sensor_msgs::CameraInfoConstPtr& info_msg) {
+/* prep the scan msg */   
+sensor_msgs::LaserScanPtr get_scan_msg(const sensor_msgs::ImageConstPtr& depth_msg,
+		const image_geometry::PinholeCameraModel cam_model_) {
 
-	image_geometry::PinholeCameraModel cam_model_;
-	cam_model_.fromCameraInfo(info_msg);
-	
-	
-	/* prep the scan msg */    // TODO: do once only instead of every frame????
 	
 	//   Calculate angle_min and angle_max by measuring angles between the left ray, right ray, and optical center ray
 	cv::Point2d raw_pixel_left(0, cam_model_.cy());
@@ -96,7 +103,7 @@ void imageCb(const sensor_msgs::ImageConstPtr& depth_msg,
 	cv::Point2d rect_pixel_bottom = cam_model_.rectifyPoint(raw_pixel_bottom);
 	cv::Point3d bottom_ray = cam_model_.projectPixelTo3dRay(rect_pixel_bottom);
 
-	double camFOVy = angle_between_rays(top_ray, bottom_ray);
+	camFOVy = angle_between_rays(top_ray, bottom_ray);
 
 	// populate laserscan message header
 	sensor_msgs::LaserScanPtr scan_msg(new sensor_msgs::LaserScan());
@@ -116,75 +123,138 @@ void imageCb(const sensor_msgs::ImageConstPtr& depth_msg,
 	uint32_t ranges_size = depth_msg->width;
 	scan_msg->ranges.assign(ranges_size, std::numeric_limits<float>::quiet_NaN());
 
+	return scan_msg;
+}
 
+
+/* extract data from depth image */
+void imageCb(const sensor_msgs::ImageConstPtr& depth_msg, 
+   const sensor_msgs::CameraInfoConstPtr& info_msg) {
+	   
+	if (framecount < DROPINITFRAMES) {
+		framecount++;
+		std::cout << "skip init frame: " << framecount << "\n";
+		return;
+	}
+
+	image_geometry::PinholeCameraModel cam_model_;
+	cam_model_.fromCameraInfo(info_msg);
 	
-	/*extract data from depth image */
+	sensor_msgs::LaserScanPtr scan_msg = get_scan_msg(depth_msg, cam_model_);
 	
 	const float center_x = cam_model_.cx();
-	const float center_y = cam_model_.cy();
+	const float center_y = cam_model_.cy(); // pixels
 
 	const double unit_scaling = 0.001f;
 	const float constant_x = unit_scaling / cam_model_.fx();
 	const float constant_y = unit_scaling / cam_model_.fy();
 
-	const uint16_t* depth_row = reinterpret_cast<const uint16_t*>(&depth_msg->data[0]);
-	int row_step = depth_msg->step / sizeof(uint16_t);
-
-	int offset = (int) (center_y - scan_height / 2);
-	depth_row += offset * row_step; // Offset to center of image
+	const int row_step = depth_msg->step / sizeof(uint16_t);
 
 	int vmax = depth_msg->height;
 	const int vfpstart = (int) (depth_msg->height * image_ignore_ratio_);
 
-	for (int v = offset; v < vmax; v++, depth_row += row_step) {
+	float hrzangleoffst;
+	double fpobstacleheight;
+	double fpcliffdepth;
 
-		// skip row if in gap between horiz and readable floor plane
-		if (v > offset + scan_height && v < vfpstart) continue;
-		if (v >= vfpstart && !(v % 4) ) continue; // floor plane skip 75% vert pixels reduce cpu
+	if (calibrationcomplete) {
+		hrzangleoffst = horiz_angle_offset_; 
+		fpobstacleheight = floorplane_obstacle_height_;
+		fpcliffdepth = floorplane_cliff_depth_;
+		framecalibrated = true;
+	} else {
+		framecalibrated = false;
+		hrzangleoffst = -HRZCALRANGE-CALIBCONST;
+		fpobstacleheight = fpcliffdepth = 0.01;
+	}
+	
+	int frameFPcount = 0;
+	int maxframeFPcount = 0;
+	float winnerhrzangleoffst = 0;
 
 
-		for (int u = 0; u < (int) depth_msg->width; u++) {
+	do {
+		const uint16_t* depth_row = reinterpret_cast<const uint16_t*>(&depth_msg->data[0]);
+		depth_row += vfpstart * row_step; // Offset to vfpstart
+		
+		for (int v = vfpstart; v < vmax; v++, depth_row += row_step) { 
 
-			if (v >= vfpstart && !(u % 4) ) continue; // floor plane skip 75% horiz pixels reduce cpu
+			if ( !(v % 2) && !framecalibrated ) continue; // floor plane skip 50% vert pixels reduce cpu
+
+			for (int u = 0; u < (int) depth_msg->width; u++) { 
+
+				// if (v >= vfpstart && !(u % 4) ) continue; // floor plane skip 75% horiz pixels reduce cpu
+						
+				uint16_t depth = depth_row[u];
+
+				double r = depth; // Assign to pass through NaNs and Infs
+				double th = -atan2((double)(u - center_x) * constant_x, unit_scaling); // Atan2(x, z), but depth divides out
+				int index = (th - scan_msg->angle_min) / scan_msg->angle_increment;
+
+				if (depth != 0) {
+					// Calculate in XYZ
+					double x = (u - center_x) * depth * constant_x;
+					double z = depth * unit_scaling;
 					
-			uint16_t depth = depth_row[u];
-
-			double r = depth; // Assign to pass through NaNs and Infs
-			double th = -atan2((double)(u - center_x) * constant_x, unit_scaling); // Atan2(x, z), but depth divides out
-			int index = (th - scan_msg->angle_min) / scan_msg->angle_increment;
-
-			if (depth != 0) {
-				// Calculate in XYZ
-				double x = (u - center_x) * depth * constant_x;
-				double z = depth * unit_scaling;
-				
-				// ignore floor plane points
-				if (v >= vfpstart) {
+					// ignore floor plane points
 					double yAngle = (v - center_y) * camFOVy/depth_msg->height;
-					yAngle += horiz_angle_offset_;
-					float fpMin = (frame_z_ - floorplane_obstacle_height_) / sin(yAngle);
-					float fpMax = (frame_z_ + floorplane_cliff_depth_) / sin(yAngle);
-					if (z>fpMin && z<fpMax)
+					yAngle += hrzangleoffst;
+					float fpMin = (frame_z_ - fpobstacleheight) / sin(yAngle);
+					float fpMax = (frame_z_ + fpcliffdepth) / sin(yAngle);
+					if (z>fpMin && z<fpMax) {
+						if (z <= 1.0) 
+							frameFPcount ++;
 						continue;  // is within floor plane, so skip use_point
-					// else if (z < fpMin) z = fpMin;
-					// else if (z > fpMax) z = fpMax;
+					}
+					
+					// Calculate actual distance
+					r = sqrt(pow(x, 2.0) + pow(z, 2.0));
 				}
 				
-				// Calculate actual distance
-				r = sqrt(pow(x, 2.0) + pow(z, 2.0));
-			}
 			
+				// Determine if this point should be used.
+				if(use_point(r, scan_msg->ranges[index], scan_msg->range_min, scan_msg->range_max)){
+					scan_msg->ranges[index] = r;
+				}
+				
+			} // end of u loop
+
+		} // end of v loop
+		
 			
-			
-			// Determine if this point should be used.
-			if(use_point(r, scan_msg->ranges[index], scan_msg->range_min, scan_msg->range_max)){
-				// std::cout << "OK to here\n";
-				scan_msg->ranges[index] = r;
-			}
-			
-			// std::cout << "OK to here\n";
+		if (frameFPcount > maxframeFPcount && !framecalibrated) {
+			maxframeFPcount = frameFPcount;
+			winnerhrzangleoffst = hrzangleoffst;
 		}
-	}
+		
+		if (!framecalibrated) {
+			std::cout << "hrzangleoffst: " << hrzangleoffst << ", frameFPcount: " << frameFPcount << "\n";
+
+			
+			hrzangleoffst += HRZCALINC;
+			
+			if (hrzangleoffst > HRZCALRANGE-CALIBCONST) { // frame calibrated
+				framecalibrated = true;
+				avghrzangleoffst += winnerhrzangleoffst;
+				calibrationscancount ++;
+				
+				std::cout << "frame calibrated: " << winnerhrzangleoffst << "\n";
+				std::cout << "maxframeFPcount: " << maxframeFPcount << "\n";
+				
+				if (calibrationscancount >= CALIBFRAMESMAX) {
+					calibrationcomplete = true;
+					horiz_angle_offset_ = avghrzangleoffst/CALIBFRAMESMAX + CALIBCONST;
+					
+					std::cout << "calibration complete " << horiz_angle_offset_ << "\n";
+				}
+				
+			}
+		}
+		
+		frameFPcount = 0;
+		
+	} while (!framecalibrated);
 	
 	pub.publish(scan_msg);
 
